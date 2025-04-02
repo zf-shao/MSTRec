935f19c77c1d6b3db000a701c0574c646c89f841
zf-shao <82147223+zf-shao@users.noreply.github.com>
2025-04-02 21:33:54 +0800

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,21 @@
+from model.bsarec import BSARecModel
+from model.caser import CaserModel
+from model.gru4rec import GRU4RecModel
+from model.sasrec import SASRecModel
+from model.bert4rec import BERT4RecModel
+from model.fmlprec import FMLPRecModel
+from model.duorec import DuoRecModel
+from model.fearec import FEARecModel
+from model.tslarec import TSLARecModel
+
+MODEL_DICT = {
+    "bsarec": BSARecModel,
+    "caser": CaserModel,
+    "gru4rec": GRU4RecModel,
+    "sasrec": SASRecModel,
+    "bert4rec": BERT4RecModel,
+    "fmlprec": FMLPRecModel,
+    "duorec": DuoRecModel,
+    "fearec": FEARecModel,
+    "tslarec": TSLARecModel,
+    }
